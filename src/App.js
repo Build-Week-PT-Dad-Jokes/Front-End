701cd58a54a4a311d40ec54d526c1e5715b2c649
AceIsHuman <menoman1995@yahoo.com>
2019-10-16 23:06:15 -0500

    move LandingPage to its own component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="landing-page-header">
-        <div className="help-button">?</div>
-      </header>
-      <div className="carousel">
-        <header className="carousel-header">
-          <span className="left-button">{'<<'}</span>
-          <h2>Sample Dad Joke Carousel</h2>
-          <span className="right-button">{'>>'}</span>
-        </header>
-        <section className="joke">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo ipsum repellat minus repellendus id illum itaque repudiandae labore rem numquam? Nihil perferendis dolor sed tenetur veniam provident perspiciatis pariatur minima.
-          </p>
-        </section>
-      </div>
+      <LandingPage />
     </div>
   );
 }
