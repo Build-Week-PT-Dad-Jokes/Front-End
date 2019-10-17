cb4e50173062e9411a53e724787e06a89de57e16
AceIsHuman <menoman1995@yahoo.com>
2019-10-16 21:54:13 -0500

    added classNames

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="landing-page-header">
+        <div className="help-button">?</div>
       </header>
+      <div className="carousel">
+        <header className="carousel-header">
+          <span className="left-button">{'<<'}</span>
+          <h2>Sample Dad Joke Carousel</h2>
+          <span className="right-button">{'>>'}</span>
+        </header>
+        <section className="joke">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo ipsum repellat minus repellendus id illum itaque repudiandae labore rem numquam? Nihil perferendis dolor sed tenetur veniam provident perspiciatis pariatur minima.
+        </section>
+      </div>
     </div>
   );
 }
