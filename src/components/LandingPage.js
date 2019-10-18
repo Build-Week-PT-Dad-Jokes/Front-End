aa6715e84d795732e2c306792dd2c4d2aac6192b
AceIsHuman <menoman1995@yahoo.com>
2019-10-17 20:45:53 -0500

    wrap LandingPage for background

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,24 +2,26 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
-      <header className="landing-page-header">
-        <div className="help-button">?</div>
-      </header>
-      <div className="carousel">
-        <header className="carousel-header">
-          <span className="left-button">{"<<"}</span>
-          <h2>Sample Dad Joke Carousel</h2>
-          <span className="right-button">{">>"}</span>
+    <div className="wrapper">
+      <div className="landing-page">
+        <header className="landing-page-header">
+          <div className="help-button">?</div>
         </header>
-        <section className="joke">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
-            ipsum repellat minus repellendus id illum itaque repudiandae labore
-            rem numquam? Nihil perferendis dolor sed tenetur veniam provident
-            perspiciatis pariatur minima.
-          </p>
-        </section>
+        <div className="carousel">
+          <header className="carousel-header">
+            <span className="left-button">{"<<"}</span>
+            <h2>Sample Dad Joke Carousel</h2>
+            <span className="right-button">{">>"}</span>
+          </header>
+          <section className="joke">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Explicabo ipsum repellat minus repellendus id illum itaque
+              repudiandae labore rem numquam? Nihil perferendis dolor sed
+              tenetur veniam provident perspiciatis pariatur minima.
+            </p>
+          </section>
+        </div>
       </div>
     </div>
   );
