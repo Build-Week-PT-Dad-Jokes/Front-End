0c3fbff72299920ba9174b09bec7dd1b7d5c6b04
AceIsHuman <menoman1995@yahoo.com>
2019-10-17 21:28:48 -0500

    move footer to bottom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import {Route} from "react-router"
+import React from "react";
+import { Route } from "react-router";
 
 //component import
-import FormikAccountForm from './components/user/CreateAccount'
-import FormikLoginForm from './components/user/Login'
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import FormikAccountForm from "./components/user/CreateAccount";
+import FormikLoginForm from "./components/user/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
-import LandingPage from './components/LandingPage';
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Route 
-        exact 
-        path="/signup" 
-        render={(props)=> 
-          <FormikAccountForm props={props} validateOnChange={false} validateOnBlur={false}/>
-        }
+      <Route
+        exact
+        path="/signup"
+        render={props => (
+          <FormikAccountForm
+            props={props}
+            validateOnChange={false}
+            validateOnBlur={false}
+          />
+        )}
       />
-      <Route 
-        exact 
-        path="/login" 
-        render={()=> 
-          <FormikLoginForm validateOnChange={false} validateOnBlur={false}/>
-        }
+      <Route
+        exact
+        path="/login"
+        render={() => (
+          <FormikLoginForm validateOnChange={false} validateOnBlur={false} />
+        )}
       />
-      <Footer />
       <Route exact path="/">
         <LandingPage />
       </Route>
+      <Footer />
     </div>
   );
 }
