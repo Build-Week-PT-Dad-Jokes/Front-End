be4f5a3790972b75b3097e9442815d926afd527e
AceIsHuman <menoman1995@yahoo.com>
2019-10-21 22:22:53 -0500

    assign response to state

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,25 +1,33 @@
-import useSetInStorage from '../hooks/useSetInStorage';
+import useSetInStorage from "../hooks/useSetInStorage";
 
 import { LOGIN_USER } from "../actions";
 
 const initialState = {
-    token: localStorage.getItem('token'),
-    userID: null,
-    username: null
+  token: localStorage.getItem("token"),
+  user: {
+    id: null,
+    username: "",
+    email: "",
+    jokes: [],
+    date_created: ""
+  }
 };
 
-export const userReducer = (state = initialState, action) => {
-  switch (action.type) {
+export const userReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
     case LOGIN_USER:
-      const [token, setToken] = useSetInStorage("token");
-      setToken(action.payload.token);
-
       return {
         ...state,
-        token: token,
-        userID: action.payload.userID,
-        username: action.payload.username
-      }
+        token: payload.token,
+        user: {
+          ...state.user,
+          date_created: payload.user.date_created,
+          email: payload.user.email,
+          id: payload.user.id,
+          jokes: payload.user.jokes,
+          username: payload.user.username,
+        }
+      };
     default:
       return state;
   }
