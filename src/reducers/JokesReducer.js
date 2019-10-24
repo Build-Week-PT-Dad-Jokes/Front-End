1e100f07ac8c9ebcd0fb1c7fe15668413a76b44e
AceIsHuman <menoman1995@yahoo.com>
2019-10-23 20:58:15 -0500

    create jokesReducer

---
+++ src/reducers/JokesReducer.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+  jokes:[],
+  isSearching: false,
+  searchResponse: null
+}
+
+export const jokesReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
+    default:
+      return state;
+  }
+}
