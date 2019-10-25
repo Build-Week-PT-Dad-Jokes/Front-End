8c8d9456030c82af9560a043f93fc1bde95d51bd
AceIsHuman <menoman1995@yahoo.com>
2019-10-24 22:07:29 -0500

    move bookmark to separte component

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,74 +1,87 @@
-import React, {useState, useEffect} from "react"
-import {BookmarkBorder, Bookmark, Share, Facebook, Twitter, ChatBubble} from '@material-ui/icons/';
-import axiosWithAuth from "../utils/axiosWithAuth"
+import React, { useState, useEffect } from "react";
+import {
+  BookmarkBorder,
+  Bookmark,
+  Share,
+  Facebook,
+  Twitter,
+  ChatBubble
+} from "@material-ui/icons/";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import BookmarkButton from "./BookmarkButton";
 
-const Joke = ({joke}) => {
-    
-    const [isBookmarked, setIsBookmarked] = useState(false)
-    const [isSharing, setIsSharing] = useState(false)
-    const [favoriteId, setFavoriteId] = useState()
 
-    useEffect(()=> {
-        updateFavorite()
-    },[isBookmarked])
 
-    const bookmark = event => {
-        setIsBookmarked(!isBookmarked)
+const Joke = ({ joke }) => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isSharing, setIsSharing] = useState(false);
+  const [favoriteId, setFavoriteId] = useState();
+
+  useEffect(() => {
+    updateFavorite();
+  }, [isBookmarked]);
+
+  const bookmark = event => {
+    setIsBookmarked(!isBookmarked);
+  };
+  const updateFavorite = () => {
+    if (isBookmarked) {
+      axiosWithAuth()
+        .post(`/favorite-joke/${joke.id}`)
+        .then(response => {
+          console.log(response);
+          setFavoriteId(response.data.favoritedJoke.id);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else if (!!favoriteId) {
+      axiosWithAuth()
+        .delete(`/favorite-joke/${favoriteId}`)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-    const updateFavorite = ()=> {
-        if(isBookmarked) {
-            axiosWithAuth()
-            .post(`/favorite-joke/${joke.id}`)
-            .then(response => {
-                console.log(response)
-                setFavoriteId(response.data.favoritedJoke.id)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }  
-        else if(!!favoriteId){
-            axiosWithAuth()
-            .delete(`/favorite-joke/${favoriteId}`)
-            .then(response => {
-                console.log(response)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }
-    }
-    const share = event => {
-        setIsSharing(!isSharing)
-    }
-    return (
-        
-        <div className="joke-card">
-            <div className="icon-flex-container">
-                <p>{!!joke.first_line && joke.first_line}</p>
-                <div onClick={bookmark} className="bookmark-button">
-                    {isBookmarked ? <Bookmark fontSize="large"/> : <BookmarkBorder fontSize="large"/>}
-                    </div>
+  };
+  const share = event => {
+    setIsSharing(!isSharing);
+  };
+  return (
+    <div className="joke-card">
+      <div className="icon-flex-container">
+        <p>{!!joke.first_line && joke.first_line}</p>
+        <BookmarkButton bookmark={bookmark} isBookmarked={isBookmarked} />
+      </div>
+      <p className="second-part">{!!joke.punchline && joke.punchline}</p>
+      <div className="icon-flex-container">
+        <p>Rating: {joke.rating}</p>
+        {!isSharing ? (
+          <div onClick={share} className="share-button">
+            <Share fontSize="large" />
+          </div>
+        ) : (
+          <div className="share-icons">
+            <div className="twitter">
+              <Twitter fontSize="large" />
             </div>
-            <p className="second-part">{!!joke.punchline && joke.punchline}</p>
-            <div className="icon-flex-container">
-                <p>Rating: {joke.rating}</p>
-                {!isSharing ? 
-                    <div onClick={share} className="share-button">
-                        <Share fontSize="large"/>
-                    </div> :
-                    <div className="share-icons">
-                        <div className="twitter"><Twitter fontSize="large"/></div>
-                        <div className="facebook"><Facebook fontSize="large"/></div>
-                        <div className="chat"><ChatBubble fontSize="large"/></div>
-                        <div onClick={share}> <Share fontSize="large" /></div>
-                    </div> }
-                
+            <div className="facebook">
+              <Facebook fontSize="large" />
             </div>
+            <div className="chat">
+              <ChatBubble fontSize="large" />
+            </div>
+            <div onClick={share}>
+              {" "}
+              <Share fontSize="large" />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-           
-        </div>
-    )
-}
-
-export default Joke
+export default Joke;
