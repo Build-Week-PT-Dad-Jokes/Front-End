42604a4c65db4e2b1c5419511f07bfa5740fb53d
AceIsHuman <menoman1995@yahoo.com>
2019-10-24 21:44:24 -0500

    add container

--- src/components/MyJokes.js
+++ src/components/MyJokes.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import Joke from './Joke';
-import AddJokeModal from './AddJokeModal';
+import Joke from "./Joke";
+import AddJokeModal from "./AddJokeModal";
 
 const MyJokes = props => {
   const { myJokes } = props;
 
-  const returnJoke = (joke, ind)=> {
-    return ind<10 &&
+  const returnJoke = (joke, ind) => {
+    return (
+      ind < 10 && (
         <div className="single-joke" key={joke.id}>
-            <Joke joke={joke} />
+          <Joke joke={joke} />
         </div>
-}
+      )
+    );
+  };
 
   return (
-    <div className="jokes-container">
-      <AddJokeModal />
-      <h2>My Jokes</h2>
-      {!!myJokes &&
-        myJokes.map((joke, ind) => returnJoke(joke, ind))}
+    <div className="home-container">
+      <div className="jokes-container">
+        <AddJokeModal />
+        <h2>My Jokes</h2>
+        {!!myJokes && myJokes.map((joke, ind) => returnJoke(joke, ind))}
+      </div>
     </div>
   );
 };
